25643182b938ee7cc5f240729d77365945a3252f
AaminaM <aamina.mariam@10pearls.com>
2022-12-21 14:41:47 +0500

    updated routes

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 require('dotenv').config();
 
 var PORT = 8000;
 
-const uri = process.env.URI;
+const { MONGO_URI } = process.env;
 
-mongoose.connect(uri, {
-    useNewUrlParser:true,
-    useCreateIndex:true
-}, ()=> {
-        console.log('Connected to the database ')
-})
+mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true,
+      //useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
 
 const routes = require('./routes/routes')
 
 app = express();
+app.use(cors(
+  {
+    credentials:true,
+    origin: ['http://localhost:8000']
+  }
+))
 app.use(express.json())
 
 app.use('/api', routes)
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
+const { SECRET } = process.env;
 
 
 router.post('/register', async (req,res) => {
@@ -12,7 +16,73 @@ router.post('/register', async (req,res) => {
         email: req.body.email,
         password: hashedPassword,
     })
-    res.send(await user.save())
-}) 
+    const result = await user.save();
+    const {password, ...data} = await result.toJSON();
+
+    res.send(data)
+})
+
+router.post('/login',async (req,res) => {
+    const user = await User.findOne({email: req.body.email})
+
+    if(!user){
+        res.status(404).send({
+            message : "User not found"
+        })
+    }
+
+    if (!await bcrypt.compare(req.body.password, user.password)){
+        res.status(400).send({
+            message : "Invalid credentials"
+        })
+    }
+
+    const token = jwt.sign({_id: user._id}, SECRET)
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000 // 1 day
+    })
+
+    res.send({
+        message: 'success'
+    })
+})
+
+
+router.get('/user',async (req,res)=> {
+    //try{
+        // const cookie = req.cookies.jwt
+        const cookie = req.cookies['jwt']
+
+        const claims = jwt.verify(cookie, SECRET)
+
+        if (!claims){
+            res.status(401).send({
+                message : "Unauthenticated 1"
+            })
+        }
+
+        const user = await User.findOne({_id:claims.id})
+        const  {password, ...data} = await user.toJSON()
+
+        //res.send(data)
+        res.send(cookie)
+    //}
+    // catch(e){
+    //     res.status(401).send({
+    //         message : "Unauthenticated 2"
+    //     })
+    // }
+})
+
+router.post('/logout', (req,res) => {
+    res.cookie('jwt','', {
+        maxAge : 0
+    })
+    res.send({
+        message: 'success'
+    })
+})
 
 module.exports =  router;
